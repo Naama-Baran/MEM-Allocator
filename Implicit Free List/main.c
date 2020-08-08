67ab0b80d5abdebadca73b89acc4cb03d83ececf
Naama-Baran <67610935+Naama-Baran@users.noreply.github.com>
2020-08-09 02:49:49 +0300

    memory allocator

---
+++ Implicit Free List/main.c
@@ -0,0 +1,64 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "malloc.h"
+#include "stdbool.h"
+
+#include "memory_allocator.h"
+
+
+void test_implicit_free_list()
+{
+	void* memoryPool = malloc(100);
+	MemoryAllocator* memoryAlloc = MemoryAllocator_init (memoryPool,100);
+	int *ptr1 = MemoryAllocator_allocate (memoryAlloc, 5 * sizeof(int));
+	int i;
+	for (i = 0; i < 5; i++)
+	{
+		*((int *)ptr1 + i) = i;
+	}
+	
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d", ptr1[i]);
+	}
+	printf("\n");
+	
+	char* ptr2 = MemoryAllocator_allocate (memoryAlloc, 24);
+	char* str = "My name is Naama Baran!";
+	
+	for (i = 0 ;str[i] != '\0'; i++)
+	{
+		ptr2[i] = str[i];
+	}
+	ptr2[i] = '\0';
+	
+	for (i = 0 ;ptr2[i] != '\0'; i++)
+	{
+		printf("%c", ptr2[i]);
+	}
+	printf("\n");
+	
+	char* ptr3 = MemoryAllocator_allocate (memoryAlloc, 30);
+	if (!ptr3)
+		printf("allocate failed\n");
+		
+	void *tmp = (size_t*)ptr1 - 1;
+	MemoryAllocator_optimize (memoryAlloc);
+	MemoryAllocator_free (memoryAlloc, ptr1);
+		
+	MemoryAllocator_free (memoryAlloc, ptr2);
+	printf("matadata: %lu\n", *((size_t*)tmp));
+	MemoryAllocator_optimize (memoryAlloc);
+	printf("matadata: %lu\n", *((size_t*)tmp));
+	
+	
+	free(MemoryAllocator_release(&memoryAlloc));
+}
+
+int main()
+{
+	test_implicit_free_list();
+	
+	
+	return 0;
+}
---
+++ Implicit Free List/memory_allocator.c
@@ -0,0 +1,156 @@
+
+#include "malloc.h"
+#include "stdbool.h"
+#include "assert.h"
+
+#include "memory_allocator.h"
+
+const static int ALIGN = 8;
+const static int MDS = sizeof(size_t);
+
+typedef struct MemoryAllocator
+{
+	void* memoryPool;
+	size_t size;
+}MemoryAllocator;
+
+//==========================================AUXILIARY FUNCTIONS==========================================
+
+static size_t align (size_t size)
+{
+	return ((size + MDS)%ALIGN == 0) ? MDS + size : MDS + size + (ALIGN - (size + MDS)%ALIGN);
+}
+
+static size_t get_size (void* ptr)
+{
+	return (*((size_t*)ptr)>>1)<<1 ;
+}
+
+static void* the_next_block (void* ptr)
+{
+	return (char*)ptr + get_size(ptr);
+}
+
+static size_t merge_two_free_blocks(void* block)
+{
+	*((size_t*)block) += get_size((the_next_block (block)));
+	printf("merged: %lu",*((size_t*)block));
+	return get_size(block);
+}
+
+static bool is_allocated(void* block)
+{
+	return *((size_t*)block)&1;
+}
+
+static void split_block(void* block, size_t size)
+{
+	 *((size_t*)((char*)block + size)) = *((size_t*)block) - size;
+	 *((size_t*)block) = size;
+}
+static void change_sign(size_t *mataData)
+{
+	*mataData = (*mataData) ^ 1;
+}
+//===================================END OF AUXILIARY FUNCTIONS======================================
+
+MemoryAllocator* MemoryAllocator_init (void *memoryPool, size_t size)
+{
+	MemoryAllocator* heap = malloc(sizeof(MemoryAllocator));
+	heap->memoryPool = memoryPool;
+	size -= ALIGN - (size)%ALIGN;
+	*((size_t*)(heap->memoryPool)) = size;
+	heap->size = size;
+	//printf("%lu\n",*((size_t*)(heap->memoryPool)));
+	return heap;
+}
+
+void * MemoryAllocator_release (MemoryAllocator** allocator)
+{
+	void* ptr = (*allocator)->memoryPool;
+	free(*allocator);
+	allocator = NULL;
+	return ptr;
+}
+
+void * MemoryAllocator_allocate (MemoryAllocator* allocator, size_t size)
+{
+	void* ptr = allocator->memoryPool;
+	size_t sizeRemain = allocator->size;
+	size_t curSize;
+	size = align(size);
+	
+	while (sizeRemain >= size)
+	{
+		curSize = get_size(ptr);
+		//printf("mataData: %lu\n", curSize);
+		//printf("size: %lu\nsizeRemain: %lu\n", size, sizeRemain);
+		if (is_allocated(ptr))
+		{
+			sizeRemain -= get_size(ptr);
+			ptr = the_next_block(ptr);
+		}
+		else
+		{
+			if (size > curSize)
+			{
+				if (!is_allocated(the_next_block(ptr)))
+				{
+					curSize = merge_two_free_blocks(ptr);
+				}
+				else
+				{
+					ptr = the_next_block(ptr);
+				}
+			}
+			if (size < curSize)
+			{
+				split_block(ptr, size);
+				curSize = get_size(ptr);
+			}
+			if (size == curSize)
+			{
+				change_sign((size_t*)(ptr));
+				return (size_t*)(ptr) + 1;
+			}
+		}
+	}
+	return NULL;
+}
+
+void MemoryAllocator_free (MemoryAllocator* allocator, void *ptr)
+{
+	assert(ptr);
+	change_sign((size_t*)ptr - 1);
+
+	if (!is_allocated(the_next_block((size_t*)ptr-1)))
+	{
+		merge_two_free_blocks((size_t*)ptr-1);
+	}
+}
+
+size_t MemoryAllocator_optimize (MemoryAllocator* allocator)
+{
+	void* ptr =  allocator->memoryPool;
+	size_t sizeRemain =  allocator->size;
+	void *nextFreeBlock;
+	size_t maxSize = 0;
+	
+	while (sizeRemain > get_size(ptr))
+	{
+		if (!is_allocated(ptr))
+		{
+			nextFreeBlock = ptr;
+			while (sizeRemain > get_size(ptr) && !is_allocated(the_next_block(nextFreeBlock)))
+			{
+				nextFreeBlock = the_next_block(nextFreeBlock);
+				merge_two_free_blocks(ptr);
+			}
+			maxSize = (get_size(ptr) > maxSize) ? get_size(ptr) : maxSize;
+		}
+		sizeRemain -= get_size(ptr);
+		ptr = the_next_block(ptr);	
+	}
+	return maxSize;
+}
+
